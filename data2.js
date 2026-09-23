5322dd3d0c0de5d62ddac1c2297c21bb7828ca96
sa234508-cell <sa234508@foonyewsa.edu.my>
2026-09-23 12:28:01 +0800

    Add files via upload

--- data2.js
+++ data2.js
@@ -1,4 +1,4 @@
-const config2 = {
+﻿const config2 = {
         showDate: true,
         showContext: true,
         showDesc: true,
@@ -48,6 +48,24 @@ const chapters2 = [
 ];
 
 const tracks2 = [
+    //Chapter 00
+    {
+        chapter: 0,
+        title: 'Toxic',
+        artist: '',
+        context: '',
+        desc: '',
+        date: '',
+        ytId: '',
+        start: 0,
+        lyrics: `
+        `,
+        lyrics_rom: `
+        `,
+        lyrics_zh: `
+        `,
+        trans_source_zh: ''
+    },
         // Chapter 01
         {
                 chapter: 1,
@@ -55,7 +73,7 @@ const tracks2 = [
                 artist: '',
                 context: '',
                 desc: '',
-                date: ',
+                date: '',
                 ytId: '',
                 start: 0,
                 lyrics: `
@@ -65,7 +83,256 @@ const tracks2 = [
                 lyrics_zh: `
         `,
                 trans_source_zh: ''
-        },
+    },
+        //Chapter 03
+    {
+        chapter: 3,
+        title: 'オルターエゴ',
+        artist: 'Vivid BAD SQUAD',
+        context: '',
+        desc: '',
+        date: '',
+        ytId: '',
+        start: 0,
+        lyrics: `現在時刻午前4時
+有象無象にもう飽き飽きだよ
+存在 形なんて
+もはやミュータントさ
+
+白に黒に馴染めず
+曖の昧な色して
+息を吸って 息を吐いて
+死んでるようなものだな
+
+実際音頼りにぎりぎり生きていた
+
+もうどうなっていいんだって
+恐れなんかは全くないって
+沈む夕日が最後は地平に
+堕ちてゆくことだけ知ってる
+
+それでもいい お前だけは
+灰になって消えてくれ
+静かに狂ったその姿に
+呑み込まれてしまう前に
+
+零でも果てでもない
+無数の目 頭の中
+ぐだぐだうだうだああうるせえな
+
+人間なんて辞めちまえ
+アダムとイヴが犯した罪の実
+善も悪も同じ
+つくりものなんだよ
+
+後ろの正面で
+誰か教えてくれたんだ
+何者にもなりたかった
+何者にもなれないから
+
+本当はもう気づいている
+許される日がこないことなんて
+咲いた花弁が最期は地上に
+堕ち塵になって風に舞ってく
+
+それでもいい お前だけは
+哀に犯され消えてくれ
+偽り纏ったその笑顔に
+呑み込まれてしまう前に
+
+色即是空の世
+無数の手 頭の中
+ぐだぐだうだうだああうるせえな
+
+孤独抱え期待なんてされなかった少年
+裏切られて傷を負った未成熟な少女も
+あぁそうか僕のことだったね
+
+君も僕で君も僕
+一人には戻れないんだよ
+
+笑ってみせてくれ
+
+それでもいい お前だけは
+青さに溺れて消えてくれ
+悲しみに満ちたその瞳に
+呑み込まれてしまう前に
+
+零で割れ世界を
+嗄れた唄口ずさんで
+フィナーレの音が鳴り響いてる
+
+最期の日 僕によく似た
+お前の生きた証を
+奪って壊して祈って焼べるよ
+今日でもう終わりなんだ
+
+灰に咲け 全てが
+朽ち果ててしまう前に
+笑顔を浮かべてお辞儀をしましょう
+
+きみがすきなうた ながれはじけた`,
+        lyrics_rom: `genzai jikoku gozen yoji
+uzoumuzou ni mou akiaki da yo
+sonzai katachi nante
+mohaya myuutanto sa
+
+shiro ni kuro ni najimezu
+ai no mai na iro shite
+iki wo sutte iki wo haite
+shinderu you na mono da na
+
+jissai oto tayori ni girigiri ikite ita
+
+mou dou natte iin datte
+osore nanka wa mattakunai tte
+shizumu yuuhi ga saigo wa chihei ni
+ochite yuku koto dake shitteru
+
+soredemo ii omae dake wa
+hai ni natte kiete kure
+shizuka ni kurutta sono sugata ni
+nomikomarete shimau mae ni
+
+zero demo hate demo nai
+musuu no me atama no naka
+gudaguda udauda aa urusee na
+
+ningen nante yamechimae
+adamu to ibu ga okashita tsumi no mi
+zen mo aku mo onaji
+tsukurimono nanda yo
+
+ushiro no shoumen de
+dareka oshiete kuretan da
+nanimono ni mo naritakatta
+nanimono ni mo narenai kara
+
+hontou wa mou kizuite iru
+yurusareru hi ga konai koto nante
+saita kaben ga saigo wa chijou ni
+ochi chiri ni natte kaze ni matteku
+
+soredemo ii omae dake wa
+ai ni okasare kiete kure
+itsuwari matotta sono egao ni
+nomikomarete shimau mae ni
+
+shikisokuzekuu no yo
+musuu no te atama no naka
+gudaguda udauda aa urusee na
+
+kodoku kakae kitai nante sarenakatta shounen
+uragirarete kizu wo otta miseijuku na shoujo mo
+aa souka boku no koto datta ne
+
+kimi mo boku de kimi mo boku
+hitori ni wa modorenain da yo
+
+waratte misete kure
+
+soredemo ii omae dake wa
+aosa ni oborete kiete kure
+kanashimi ni michita sono hitomi ni
+nomikomarete shimau mae ni
+
+zero de ware sekai wo
+shagareta uta kuchizusande
+finaare no oto ga narihibiiteru
+
+saigo no hi boku ni yoku nita
+omae no ikita akashi wo
+ubatte kowashite inotte kuberu yo
+kyou de mou owari nanda
+
+hai ni sake subete ga
+kuchihatete shimau mae ni
+egao wo ukabete ojigi wo shimashou
+
+kimi ga suki na uta nagare hajiketa`,
+        lyrics_zh: `
+        `,
+        trans_source_zh: '',
+        lyrics_en: `The present time is 4 AM,
+I'm sick and tired of everyone.
+The shape of my existence
+Has already mutated.
+
+Without being graded into black or white,
+My colors become vague.
+Inhale, then exhale,
+It's almost as if I'm dead.
+
+But in reality, if one listens, I'm just about alive.
+
+I say that I don't care what happens anymore,
+That I don't have single drop of fear.
+But the only thing I know is that the end will be
+The evening sun sinking below the horizon.
+
+But that's fine, just if you'd
+Turn to ash and disappear,
+Before we're swallowed by
+That figure that quietly went insane.
+
+With neither a zero or an end,
+The countless eyes in my head
+They blabber, going on and on and on. Oh just shut up.
+
+How about you just quit being human?
+The moral of Adam and Eve and the sin they committed,
+Is that good and evil are
+Just man-made fabrications.
+
+Someone once told me that
+Standing right behind me.
+I wanted to make something of myself,
+Exactly because I can't do just that.
+
+The truth is I already know that
+The day I'm forgiven will never come.
+The final moments of a bloomed petal are when it falls
+To the ground to become dirt, whirling in the wind.
+
+But that's fine, just if you'd
+Get broken by sorrow and disappear.
+Before we're swallowed by
+That smile coated with lies.
+
+A world where matter is void,
+The countless hands in my head.
+They blabber, going on and on and on. Oh just shut up.
+
+Both the boy carrying loneliness who wasn't allowed to hope,
+And the naïve girl who was betrayed and had to bear the wounds,
+Oh, I see. They were both me.
+
+You are me, and you are also me.
+I can't go back to being on my own.
+
+Please show me a smile.
+
+But it's fine, just if you'd
+Drown in the blue and disappear.
+Before we're swallowed by
+Those eyes filled with pain.
+
+At zero, I hum a hoarse song
+About the broken world.
+And the sound of the finale rings out.
+
+The day before death, I'll take the proof
+Of your existence, so similar to mine.
+I'll steal it away, destroy it, then pray, and burn it.
+This all ends today, after all.
+
+Blooming from the ash,
+Before everything crumbles away,
+Give me a smile and let's bow a greeting.
+
+That song you liked so much streamed out.`,
+        trans_source_en:`Project SEKAI Fandom Wiki | Translate by Anonymous and Migu39`,
+    },
 ];
 
 // Universal aliases for accessibility and namespace safety
